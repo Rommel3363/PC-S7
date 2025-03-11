516424743f5559c79522bf8eb6ab44cc710a2bc6
orange <786924052@qq.com>
2025-03-11 14:02:53 +0800

    用的个人电脑，把IP设定为固定IP就能成了。距离范围是65536

--- S7.py
+++ S7.py
@@ -1,6 +1,22 @@
 import snap7
 from snap7.util import *
 
+def turnToDoubleByte(num):
+    if num<256:
+        result = bytearray([0,num])
+        return result
+    else:
+        binary = bin(num)[2:]
+            # 删去低8位的0
+        highEightB = binary[:-8]
+        lowEightB = binary[-8:]
+
+        highEightO = int(highEightB,2)
+        lowEightO = int(lowEightB,2)
+        
+        result = bytearray([highEightO,lowEightO])
+        return result
+
 # 定义PLC的IP地址
 PLC_IP = '192.168.0.211'  # 替换为你的PLC的IP地址
 
@@ -16,12 +32,19 @@ if client.get_connected():
     # 写入数据到PLC DB块
     db_number = 1  # DB块号
     start = 0  # 起始地址
-    data = bytearray([123])  # 要写入的数据，示例为写入整数123
+    distence = 4396 # to add to json
+    data = turnToDoubleByte(distence)
+    # data = bytearray([0,distence])  # 要写入的数据，示例为写入整数123
 
-    client.db_write(db_number, start, data)
+    resu = client.db_write(db_number, start, data)
     print("成功写入数据")
 
     # 断开连接
     client.disconnect()
 else:
     print("无法连接到PLC")
+
+
+
+        
+    
